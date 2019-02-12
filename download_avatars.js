ffaee8064b90cdc99e8b092ac530f2a72e86ba9c
amilford87 <amilford87@gmail.com>
2019-02-12 01:19:23 +0000

    linted

--- download_avatars.js
+++ download_avatars.js
@@ -5,40 +5,42 @@ var arg = process.argv.slice(2);
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+//Error message if arguments are undefined and getting the repo contributors via github.com/repos
 function getRepoContributors(repoOwner, repoName, cb) {
-    if (arg[0] === undefined || arg[1] === undefined) {
+    if (repoOwner === undefined || repoName === undefined) {
         console.log("Please input a propery user and repo.");
+    } else {
+        var options = {
+        url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+        headers: {
+            'User-Agent': 'request',
+        }
+        };
+        
+        //parsing through the contributors information to only collect the avatar URLS
+        request(options, function(err, res, body) {
+            var bodyContent = JSON.parse(body);
+            cb(err, bodyContent);
+            bodyContent.forEach(function (contributor){
+                var avatarUrls = contributor.avatar_url;
+                downloadImageByURL(avatarUrls, `avatars/${contributor.login}jpg`);
+            });
+        });
     }
-    else {
-    var options = {
-       url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-       headers: {
-           'User-Agent': 'request',
-       }
-
-   };
-
-
-   request(options, function(err, res, body) {
-       cb(err, body);
-       var bodyContent = JSON.parse(body);
-    bodyContent.forEach(function (contributor){
-        var avatarUrls = contributor.avatar_url;
-        downloadImageByURL(avatarUrls, `avatars/${contributor.login}jpg`);
-    });
-   });
-  }
 }
 
-  function downloadImageByURL(url, filePath) {
+//the function that actually writes the image from the urls to a filepath
+function downloadImageByURL(url, filePath) {
     request.get(url)
     
     .on('error', function(err){
         throw err;
     })
+    
     .on('response', function(response){
         console.log("Status: " + response.statusMessage);
     })
+    
     .on('data', function(data){
         console.log('Downloading image...');
     })
@@ -47,11 +49,11 @@ function getRepoContributors(repoOwner, repoName, cb) {
     
     .on('finish', function (response){
         console.log("Download complete");
-  });
+    });
 }
 
+//final function call to pull the callback functions from whatever user and repo is entered.
 getRepoContributors(arg[0], arg[1], function(err, result) {
     console.log("Errors:", err);
     console.log(result);
-    
-  });
+});
