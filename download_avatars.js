dcf8c6711e8a03119f5266d69a63ead5d740db76
amilford87 <amilford87@gmail.com>
2019-02-12 00:04:09 +0000

    only pushing out avatar urls

--- download_avatars.js
+++ download_avatars.js
@@ -4,19 +4,24 @@ var secrets = require('./secrets.js');
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-   var options = {
+    var options = {
        url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
        headers: {
            'User-Agent': 'request',
        }
+
    };
 
    request(options, function(err, res, body) {
        cb(err, body);
+       var bodyContent = JSON.parse(body);
+    bodyContent.forEach(function (contributor){
+        console.log(contributor.avatar_url);
+    });
    });
   }
 
   getRepoContributors("jquery", "jquery", function(err, result) {
     console.log("Errors:", err);
-    console.log("Result:", result);
+    // console.log("Result:", result);
   });
